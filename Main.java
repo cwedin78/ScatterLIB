86595032d3f5e90b697c6db25358577d7b836f8e
cwedin78 <55719118+cwedin78@users.noreply.github.com>
2022-05-26 23:56:41 -0500

    Lines creating properly

--- Main.java
+++ Main.java
@@ -4,11 +4,12 @@ import src.ScatterPlot.Point;
 public class Main {
     public static void main(String[] args) {
         ScatterPlot plot = new ScatterPlot();
-        plot.AddPoints(new Point(3, 5, 5));
-        plot.AddPoints(new Point(0.25, 8, 5));
-        plot.AddPoints(new Point(1, 6, 5));
-        plot.AddPoints(new Point(.5, 1, 5));
-        plot.AddPoints(new Point(4,6,6));
+        plot.AddPoints(
+             new Point(3, 5, 5)
+            ,new Point(1, 6, 5)
+            ,new Point(2, 3, 5)
+            ,new Point(2.1, 7, 5)
+        );
         System.out.println(plot.Calculate(3.3,1));
     }
 }
